e31b662925b0844a891b9ad3584233c823c277e0
shawon <rabby.shawon@vivasoftltd.com>
2024-09-11 10:41:05 +0600

    Added slider in home screen

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,12 +1,16 @@
 import { View, Text } from "react-native";
 import Header from "../../components/Home/Header";
-import Slider from "../../components/Home/Slider";
+import Slider from "@/components/Home/Slider";
+import Category from "@/components/Home/Category";
+import Petlist from "@/components/Home/Petlist";
 
 export default function Home() {
   return (
     <View style={{ padding: 20, marginTop: 20 }}>
       <Header />
       <Slider />
+      <Category />
+      <Petlist />
     </View>
   );
 }
--- components/Home/Slider.jsx
+++
@@ -1,9 +0,0 @@
-import { View, Text } from "react-native";
-
-export default function Slider() {
-  return (
-    <View>
-      <Text>Slider</Text>
-    </View>
-  );
-}
